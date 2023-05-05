62891b88a48de19939b901b83a41f3343df85589
Enrique Hernandez <enrique_inf@hotmail.com>
2023-05-04 19:25:00 -0600

    primer commit

---
+++ Xamarin2018/Xamarin2018/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin2018
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(usuarioEntry.Text) || string.IsNullOrEmpty(passwordEntry.Text))
+            {
+                resultadoLabel.Text = "Debes escribir usuario y contraseña";
+            }
+            else
+            {
+                resultadoLabel.Text = "Inicio de sesion exitoso";
+                await Navigation.PushAsync(new NuevoItem());
+            }
+        }
+    }
+}
